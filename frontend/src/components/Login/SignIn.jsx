9724293f1898022e04f8ee553b4d49fd4ca3463b
ayush-999 <ayushchaturvedi789@gmail.com>
2024-07-22 23:38:34 +0530

    branch issue fix

--- frontend/src/components/Login/SignIn.jsx
+++ frontend/src/components/Login/SignIn.jsx
@@ -75,9 +75,9 @@ const SignInPage = () => {
                       id="email"
                       className={`bg-gray-50 border ${
                         errors.email && touched.email
-                          ? "border-error-600 bg-error-0 focus:border-error-600"
+                          ? "border-error-600 bg-error-10 focus-within:border-error-600"
                           : "border-gray-300"
-                      } text-gray-900 rounded-lg focus:border-primary-600 block w-full p-2.5`}
+                      } text-gray-900 rounded-lg focus-within:border-primary-600 block w-full p-2.5`}
                       placeholder="name@company.com"
                       autoComplete="email"
                     />
@@ -100,9 +100,9 @@ const SignInPage = () => {
                       id="password"
                       className={`bg-gray-50 border ${
                         errors.password && touched.password
-                          ? "border-error-600 bg-error-0 focus:border-error-600"
+                          ? "border-error-600 bg-error-10 focus-within:border-error-600"
                           : "border-gray-300"
-                      } text-gray-900 rounded-lg focus:border-primary-600 block w-full p-2.5 input-with-icon`}
+                      } text-gray-900 rounded-lg focus-within:border-primary-600 block w-full p-2.5 input-with-icon`}
                       placeholder="••••••••"
                       autoComplete="current-password"
                     />
--- frontend/src/components/Register/PersonalInfo.jsx
+++ frontend/src/components/Register/PersonalInfo.jsx
@@ -27,9 +27,9 @@ const PersonalInfo = ({
             id="email"
             className={`bg-gray-50 border ${
               errors.email && touched.email
-                ? "border-error-600 bg-error-0 focus:border-error-600"
+                ? "border-error-600 bg-error-10 focus-within:border-error-600"
                 : "border-gray-300"
-            } text-gray-900 rounded-lg focus:border-primary-600 block w-full p-2.5`}
+            } text-gray-900 rounded-lg focus-within:border-primary-600 block w-full p-2.5`}
             placeholder="Enter your email"
             autoComplete="email"
           />
@@ -51,9 +51,17 @@ const PersonalInfo = ({
             id="userMobile"
             defaultCountry="in"
             value={values.userMobile}
-            onChange={(phone) => setFieldValue("userMobile", phone)}
-            onBlur={() => setFieldTouched("userMobile", true)}
-            className='userMobile'
+            onChange={(phone) => {
+              setFieldValue("userMobile", phone);
+            }}
+            onBlur={() => {
+              setFieldTouched("userMobile", true);
+            }}
+            className={`userMobile ${
+              errors.userMobile && touched.userMobile
+                ? "error-show"
+                : "border-gray-300"
+            }`}
           />
           <ErrorMessage
             name="userMobile"
